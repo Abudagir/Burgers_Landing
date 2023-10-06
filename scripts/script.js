6032131476f90ad44730048a61b9e158e3694550
Abudagir <kondaktor3@gmail.com>
2023-10-06 23:05:46 +0200

    final

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,21 @@
 document.getElementById("main-action-button").onclick = function () {
-  document.getElementById("products").scrollIntoView({behavior: "smooth"});
-}
+  document.getElementById("products").scrollIntoView({ behavior: "smooth" });
+};
 
 let links = document.querySelectorAll(".menu-item > a");
 for (let i = 0; i < links.length; i++) {
   links[i].onclick = function () {
-      document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior: "smooth"});
-  }
+    document
+      .getElementById(links[i].getAttribute("data-link"))
+      .scrollIntoView({ behavior: "smooth" });
+  };
 }
 
 let buttons = document.getElementsByClassName("product-button");
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].onclick = function () {
-      document.getElementById("order").scrollIntoView({behavior: "smooth"});
-  }
+    document.getElementById("order").scrollIntoView({ behavior: "smooth" });
+  };
 }
 
 let burger = document.getElementById("burger");
@@ -23,22 +25,22 @@ let phone = document.getElementById("phone");
 document.getElementById("order-action").onclick = function () {
   let hasError = false;
 
-  [burger, name, phone].forEach(item => {
-      if (!item.value) {
-          item.parentElement.style.background = "red";
-          hasError = true;
-      } else {
-          item.parentElement.style.background = "";
-      }
+  [burger, name, phone].forEach((item) => {
+    if (!item.value) {
+      item.parentElement.style.background = "red";
+      hasError = true;
+    } else {
+      item.parentElement.style.background = "";
+    }
   });
 
   if (!hasError) {
-      [burger, name, phone].forEach(item => {
-          item.value = "";
-      });
-      alert("Спасибо за заказ! Мы скоро свяжемся с вами!")
+    [burger, name, phone].forEach((item) => {
+      item.value = "";
+    });
+    alert("Thank you for your order! We will contact you shortly!");
   }
-}
+};
 let prices = document.getElementsByClassName("products-item-price");
 document.getElementById("change-currency").onclick = function (e) {
   let currentCurrency = e.target.innerText;
@@ -47,22 +49,18 @@ document.getElementById("change-currency").onclick = function (e) {
   let coefficient = 1;
 
   if (currentCurrency === "$") {
-      newCurrency = "₽";
-      coefficient = 96;
-  } else if (currentCurrency === "₽") {
-      newCurrency = "BYN";
-      coefficient = 3;
-  } else if (currentCurrency === "BYN") {
-      newCurrency = "€";
-      coefficient = 0.94;
+    newCurrency = "€";
+    coefficient = 0.94;
   } else if (currentCurrency === "€") {
-     newCurrency = '¥';
-     coefficient = 6.9;
+    newCurrency = "¥";
+    coefficient = 6.9;
   }
   e.target.innerText = newCurrency;
 
   for (let i = 0; i < prices.length; i++) {
-      prices[i].innerText = +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) + " " + newCurrency;
+    prices[i].innerText =
+      +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) +
+      " " +
+      newCurrency;
   }
-
-}
+};
